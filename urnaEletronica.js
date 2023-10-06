0a7e57628f5524b3b10d42adb31af2d8ba927942
KappaD <121440624+KapaaD@users.noreply.github.com>
2023-10-06 19:47:20 +0000

    é sobre isso

---
+++ urnaEletronica.js
@@ -0,0 +1,36 @@
+// // Estrutura: switch-case
+
+// switch {
+
+//     case 1:
+//         console.log('Opção é 1');
+//         break;
+//     case 2:
+//         console.log('Opção é 2');
+//         break;
+//     default:
+//         console.log('Nenhuma das opções');
+//         break;
+    
+// }
+
+// // equivalente a estrutura switch-case
+// if (opcao === 1){
+//     console.log('Opção é 1'); 
+// }else if ('opcao é 2') {
+//     console.log('Opção é 2');
+// }else {
+//     console.log('Nenhuma das opções')
+// } 
+
+// Estrutura de repetição: do-while
+
+
+// Estrutura de repetição: while (enquanto)
+
+let contador = 0;
+do {
+    console.log('Instrução com o contador em', contador);
+    contador += 1;
+while (contador)
+} while (opcao <= 10);
